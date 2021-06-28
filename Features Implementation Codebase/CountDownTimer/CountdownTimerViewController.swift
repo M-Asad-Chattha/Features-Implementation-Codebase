46efa74eb707e79087080ccb7b74c7db4aebcc6d
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2021-06-28 22:53:46 +0500

    Implement ProgressView, which increases with timer.

--- Features Implementation Codebase/CountDownTimer/CountdownTimerViewController.swift
+++ Features Implementation Codebase/CountDownTimer/CountdownTimerViewController.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class CountdownTimerViewController: UIViewController {
 
-    var countdownTime = 7
-        
     @IBOutlet weak var remainingTimeLabel: UILabel!
+    @IBOutlet weak var timerProgressView: UIProgressView!
+    
+    var totalTime = 7
+    var countdownTime = 7
+    var secondsPassed = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        ///Update remainingTimeLabel with provided value like; 7
-        remainingTimeLabel.text = "7"
     }
     
-    //MARK: - Countdown Timer [Countdown time from a specific number to 1 (like; 7...1)]
+    //MARK: - Countdown Timer
     
     @IBAction func startCountdownTimer(_ sender: UIButton) {
-        
         ///withTimeInterval: 1.0 means 1second
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
             
@@ -37,14 +36,31 @@ class CountdownTimerViewController: UIViewController {
      - Parameter timer: contain Timer object refrence passed by Closure.
      */
     func updateTimer(timer: Timer) {
-        if self.countdownTime > 0 {
+        ///Countdown time from a specific number to 1 (like; 7...1)
+        if secondsPassed < totalTime {
             remainingTimeLabel.text = "\(countdownTime)"
             countdownTime -= 1
+            secondsPassed += 1
+            updateProgressView()
         } else {
             timer.invalidate()
-            self.remainingTimeLabel.text = "Timer ends❗️"
-            self.countdownTime = 7 //Reset time
+            playSound()
+            resetTime()
         }
     }
     
+    /**
+     Reset all timer & progressView variables to their startup state
+     */
+    func resetTime() {
+        remainingTimeLabel.text = "Timer ends❗️"
+        countdownTime = 7 //Reset timer
+        secondsPassed = 0 //Reset seconds
+    }
+    
+    //MARK: - Progress View
+    func updateProgressView() {
+        let percentageProgress = Float(secondsPassed) / Float(totalTime)
+        timerProgressView.progress = percentageProgress
+    }
 }
